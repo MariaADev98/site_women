09adfcb6f398cbe080c6125cdf198aef76bc35d4
MariaADev98 <bulatova2798@gmail.com>
2023-11-29 13:41:59 +0300

    uraaaa

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sitewomen project.
+URL configuration for sitwoman project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from women.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
+handler404 = page_not_found
