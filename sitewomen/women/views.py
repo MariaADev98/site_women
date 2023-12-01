a9ff0eed4809911e74edb1892a6905b0f8e539e1
MariaADev98 <bulatova2798@gmail.com>
2023-12-01 20:51:39 +0300

    uraaaa

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
+
+from .models import Women
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статью', 'url_name': 'add_page'},
@@ -26,11 +28,10 @@ cats_db = [
 
 
 def index(request):
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
+    posts = Women.objects.filter(is_published=1)
     data = {'title': 'Главная страница',
             'menu': menu,
-            'posts': data_db,
+            'posts': posts,
             'cat_selected': 0,
 
             }
@@ -41,8 +42,16 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
@@ -62,7 +71,7 @@ def show_category(request, cat_id):
             'menu': menu,
             'posts': data_db,
             'cat_selected': cat_id,
-    }
+            }
     return render(request, 'women/index.html', context=data)
 
 
