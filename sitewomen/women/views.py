76ab2be6970a71a3903e077ff6d3af4695b97bc7
MariaADev98 <bulatova2798@gmail.com>
2023-12-02 18:35:09 +0300

    uraaaa

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
 from .models import Women, Category
 
