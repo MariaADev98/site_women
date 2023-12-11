5dc11e20632005022c24f5c3697ff3040d61a819
MariaADev98 <bulatova2798@gmail.com>
2023-12-11 22:06:26 +0300

    uraaaa

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'women.apps.WomenConfig',
     'users',
     'debug_toolbar',
+    'social_django',
+
 
 ]
 
@@ -138,6 +140,7 @@ LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'users:login'
 
 AUTHENTICATION_BACKENDS = [
+    'social_core.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 
@@ -161,3 +164,19 @@ AUTH_USER_MODEL = 'users.User'
 
 DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
 
+SOCIAL_AUTH_GITHUB_KEY = 'd6e73791e0c1627f5176'
+SOCIAL_AUTH_GITHUB_SECRET = '8e23b904a744f394e9b3df59280e30dcf8770c8b'
+
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.new_users_handler',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
