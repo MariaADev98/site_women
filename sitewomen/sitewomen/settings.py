a6920b8fc366cb12861d444f58b3a49d2a91bee8
MariaADev98 <bulatova2798@gmail.com>
2024-01-06 20:40:15 +0300

    arsssss

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -110,13 +110,6 @@ DATABASES = {
     # }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
